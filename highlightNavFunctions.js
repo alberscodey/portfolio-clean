544c4c3be3e0e6edea90e202e635b308242d3051
Codey Albers <45026031+alberscodey@users.noreply.github.com>
2019-08-23 06:31:12 -0400

    fixed the scroll function, made progress bars level up

--- highlightNavFunctions.js
+++ highlightNavFunctions.js
@@ -1,22 +1,59 @@
 //First, let's identify which elements need to be highlighted as you scroll.
 const nav = document.getElementById('nav');
+
+const landing = document.getElementById('home');
+
 const skillsNavElement = document.getElementById('skills-nav-element');
+const figmaProgress = document.getElementById('figma-progress');
+const htmlProgress = document.getElementById('html-progress');
+const javascriptProgress = document.getElementById('javascript-progress');
+const adobeProgress = document.getElementById('adobe-progress');
+
 const designsNavElement = document.getElementById('designs-nav-element');
 
 //Okay, now as you scroll into a section of the site, a new class will be added to its corresponding nav element
+//Landing
+function enterLanding() {
+    nav.classList.remove('portfolio-gradient');
+}
 
 //Skills
 function enterSkills() {
-    /*nav.classList.add('portfolio-gradient');*/
+    nav.classList.add('portfolio-gradient');
     skillsNavElement.classList.add('highlighted');
+    
+    //when you scroll into the skills section, the progress bars fill up
+    figmaProgress.classList.remove('shrink');
+    figmaProgress.classList.add('figma-grow');
+
+    htmlProgress.classList.remove('shrink');
+    htmlProgress.classList.add('html-grow');
+
+    javascriptProgress.classList.remove('shrink');
+    javascriptProgress.classList.add('javascript-grow');
+
+    adobeProgress.classList.remove('shrink');
+    adobeProgress.classList.add('adobe-grow');
 };
 function exitSkills() {
-    /*nav.classList.remove('portfolio-gradient');*/
     skillsNavElement.classList.remove('highlighted');
+
+    figmaProgress.classList.remove('figma-grow');
+    figmaProgress.classList.add('shrink');
+
+    htmlProgress.classList.remove('html-grow');
+    htmlProgress.classList.add('shrink');
+
+    javascriptProgress.classList.remove('javascript-grow');
+    javascriptProgress.classList.add('shrink');
+
+    adobeProgress.classList.remove('adobe-grow');
+    adobeProgress.classList.add('shrink');
 }
 
 //Designs
 function enterDesigns() {
+    nav.classList.add('portfolio-gradient');
     designsNavElement.classList.add('highlighted');
 };
 function exitDesigns() {
@@ -45,12 +82,9 @@ function enterFinishIt() {
 }
 function exitFinishIt() {
     nav.classList.remove('purple');
-}
+}*/
 
 //Contact
 function enterContact() {
     nav.classList.add('portfolio-gradient');
 }
-function exitContact() {
-    nav.classList.remove('portfolio-gradient');
-} */
